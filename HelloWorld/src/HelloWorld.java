9fa8494496dccea3e339eb50d84081b0b020950c
Ebner.Lars <Ebner.Lars@SK-448A5B8FB5E0.2631.skool.net>
2017-09-19 15:32:15 +0200

    Hello World was changed to a programm for the collantz problem

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,9 +1,24 @@
-
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-
+		
+	for(int i = 1; i<= 100; i++ )
+		/*In dieser for Schleife wird festgelegt für welchen Zahlenbereichdie Collazvermutung durchgeführt wird **/
+	{
+		int n = i;
+		System.out.print(n +": ");
+		
+	do{
+		if(n%2==0)
+		{n=n/2;}
+		else 
+		{n=3*n+1;}
+		
+		System.out.print(n +" ");}
+		while(n>1);
+		System.out.println();
+			}
+		}
 	}
 
-}
+
