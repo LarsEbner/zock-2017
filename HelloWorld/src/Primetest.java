4e502908b45586f6f7b498a0e378aff162ea1b8a
Ebner.Lars <Ebner.Lars@SK-448A5B8FB5E0.2631.skool.net>
2017-10-06 08:40:32 +0200

    prime+collatz

--- HelloWorld/src/Primetest.java
+++ HelloWorld/src/Primetest.java
@@ -1,15 +1,35 @@
 public class Primetest {
-	static int threshold = 2000000;
+	static int threshold = 1000;
 	
 	public static void main(String[] args) {
 		
 		
 		primtest(threshold);
-	//	collatz(7);
+		collatz(7);
 		
 		
 		
 	}
+	
+	
+	private static void collatz(int i){
+		System.out.println();
+		System.out.println("Collatzfolge von "+i+":");
+			int n = i;   // hier wird die Hochzählvariable 
+						// nach n kopiert, damit diese durch 
+						//folgenden Algorithmus nicht überschrieben wird
+			System.out.print(n +": "); // Ausgabe der berechneten Collatz-Startzahl
+		
+			do{							// Beginn des Collatz Algorithmus...
+				if(n%2==0){
+					n=n/2;}
+				else{n=3*n+1;}
+			
+			System.out.print(n +" ");} //und Ausgabe
+			while(n>1);  				// ..solange n >1
+			System.out.println();
+		 }
+	
 	
 	
 	private static boolean prime(int number){
