46aacefb8f3a2156d0e68af982ad1df72485f83b
Ebner.Lars <Ebner.Lars@SK-448A5B8FB5E0.2631.skool.net>
2017-10-06 09:12:32 +0200

    change to game

--- zock 2017/src/Vertex.java
+++ zock 2017/src/Vertex.java
@@ -1,4 +1,21 @@
 
 public class Vertex {
 
+	public double x;
+	public double y;
+	
+	public Vertex(double x, double y){
+	
+		this.x = x;
+		this.y = y;
+		
+		
+		
+	}
+		
+		public static void main(String[] args) {
+	
+			Vertex v1 = new Vertex(2,2);
+			System.out.println(v1);
+}
 }
